2e9127a48f4adf28a17832f8ed2b5a96315d67b1
amanpatel00481 <161560648+amanpatel00481@users.noreply.github.com>
2024-03-23 12:37:56 +0530

    Update AdminController.java

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -20,7 +20,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import java.util.List;
 
 @Controller
-public class AuthController {
+public class AdminController {
 
     private UserService userService;
     private RoleServiceimpl roleService;
